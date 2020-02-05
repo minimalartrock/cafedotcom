ef1077071cf7018f5c803b5fc6ad9fe8b1bcce74
minimalartrock <minimal.art.rock.5@gmail.com>
2020-02-05 21:09:28 +0900

    ファイルアップロード機能を実装した

---
+++ db/migrate/20200205111946_remove_images_from_shops.rb
@@ -0,0 +1,7 @@
+class RemoveImagesFromShops < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :shops, :image_1, :string
+    remove_column :shops, :image_2, :string
+    remove_column :shops, :image_3, :string
+  end
+end
