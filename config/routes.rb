6daf8b8ec995985f8d7ab27b1a282aec80ffc0aa
minimalartrock <minimal.art.rock.5@gmail.com>
2020-02-09 15:43:46 +0900

    ログイン機能を実装した

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get "users/show"
   devise_for :users, controllers: {
-    omniauth_callbacks: 'users/omniauth_callbacks'
-  }
-	root to: 'shops#index'
-	resources :shops
+                       omniauth_callbacks: "users/omniauth_callbacks",
+                       registrations: "registrations",
+                     }
+  root to: "shops#index"
+  resources :shops
+  resources :users, only: [:index, :show]
 end
