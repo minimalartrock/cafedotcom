3589a603d2949d648ef583b22c1a147e5bf4431a
minimalartrock <minimal.art.rock.5@gmail.com>
2020-03-03 20:09:42 +0900

    - Comment.commentの最大サイズを変更した - 投稿ボタンの表示を変更した

--- db/migrate/20200209080650_create_comments.rb
+++ db/migrate/20200209080650_create_comments.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-      t.text :comment, null: false
+      t.text :comment, limit: 300, null: false
       t.references :shop, foreign_key: true, null: false
       t.references :user, foreign_key: true, null: false
       t.timestamps
