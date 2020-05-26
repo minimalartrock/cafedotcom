6231f8b08cd40ec80ad0a36346f5bd30db47d0fe
minimalartrock <minimal.art.rock.5@gmail.com>
2020-05-26 22:21:03 +0900

    Change search request format to ajax

--- test/controllers/search_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class SearchControllerTest < ActionDispatch::IntegrationTest
-  test "should get prefecture" do
-    get search_prefecture_url
-    assert_response :success
-  end
-
-end
