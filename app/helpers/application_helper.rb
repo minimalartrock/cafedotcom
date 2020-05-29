11295f28eb32e285ca7655f1a499d70a74c57bb3
minimalartrock <minimal.art.rock.5@gmail.com>
2020-05-29 21:21:31 +0900

    Fix meta data

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,36 @@
 module ApplicationHelper
   require 'uri'
 
+	APP_TITLE = 'カフェどっと混む'
+	APP_URL = 'https://cafedotcom.jp/'
+	APP_DESCRIPTION = 'カフェの混雑状況が確認できるサイトです。お店の情報や口コミを見ることができます。'
+	APP_THUMBNAIL = 'https://cafedotcom.jp/images/thumbnail.png'
+
+	def full_title(page_title = '')
+		base_title = APP_TITLE
+		if page_title.empty?
+			base_title
+		else
+			"#{page_title} | #{base_title}"
+		end
+	end
+
+	def og_url(page_url = '')
+		page_url.empty? ? APP_URL : page_url
+	end
+
+	def og_title(page_title = '')
+		page_title.empty? ? APP_TITLE : page_title
+	end
+
+	def og_description(page_description = '')
+		page_description.empty? ? APP_DESCRIPTION : page_description
+	end
+
+	def og_image(page_image = '')
+		page_image.empty? ? APP_THUMBNAIL : page_image
+	end
+
   def text_url_to_link(text)
     URI.extract(text, %w[http https]).uniq.each do |url|
       sub_text = ''
